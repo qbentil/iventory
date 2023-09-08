1719b8bf301fb496cbf8d4012ba0717c6c32f457
qbentil <bentilshadrack72@gmail.com>
2023-09-08 18:19:30 +0000

    Setup a base App layout

---
+++ App/layout.tsx
@@ -0,0 +1,27 @@
+import { AiFillTags, AiOutlineStock } from 'react-icons/ai';
+import { Authenticate, Footer, Header, Navigations } from '@/components'
+
+import React from 'react'
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <Authenticate >
+            {/*  */}
+            <div className='w-screen h-screen flex flex-col  bg-green-600'>
+                {/* header */}
+                <Header />
+
+                {/* navs */}
+                <Navigations />
+                {/* children */}
+                {children}
+
+
+                {/* footer */}
+                <Footer />
+            </div>
+        </Authenticate>
+    )
+}
+
+export default RootLayout
