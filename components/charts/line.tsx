3f20b89fa057b7c993e5d5a24cbf6e41326a86ae
qbentil <bentilshadrack72@gmail.com>
2023-09-18 13:45:05 +0000

    Line Chart component

---
+++ components/charts/line.tsx
@@ -0,0 +1,80 @@
+import {
+    CategoryScale,
+    Chart as ChartJS,
+    Legend,
+    LineElement,
+    LinearScale,
+    PointElement,
+    Title,
+    Tooltip,
+} from 'chart.js';
+
+import { Line } from 'react-chartjs-2';
+import React from 'react';
+import { faker } from "@faker-js/faker"
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+export const options = {
+    responsive: true,
+    interaction: {
+        mode: 'index' as const,
+        intersect: false,
+    },
+    stacked: false,
+    plugins: {
+        title: {
+            display: true,
+            text: 'Monthly Expenses (GHC)',
+        },
+    },
+    scales: {
+        y: {
+            type: 'linear' as const,
+            display: true,
+            position: 'left' as const,
+        },
+        y1: {
+            type: 'linear' as const,
+            display: true,
+            position: 'right' as const,
+            grid: {
+                drawOnChartArea: false,
+            },
+        },
+    },
+};
+
+const labels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'];
+
+export const data = {
+    labels,
+    datasets: [
+        {
+            label: 'Item 1',
+            data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            yAxisID: 'y',
+        },
+        {
+            label: 'Item 2',
+            data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+            borderColor: 'rgb(53, 162, 235)',
+            backgroundColor: 'rgba(53, 162, 235, 0.5)',
+            yAxisID: 'y1',
+        },
+    ],
+};
+
+export default function LineChart() {
+    return <Line options={options} data={data} />;
+}
