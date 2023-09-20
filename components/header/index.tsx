cea8d0774e86d798c9a023e0b054dc15530b13bd
qbentil <bentilshadrack72@gmail.com>
2023-09-20 11:17:47 +0000

    Add dropdown

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,5 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
 
+import { PiCaretDownBold, PiUserSwitchThin } from 'react-icons/pi'
+
+import { AiOutlineSetting } from 'react-icons/ai'
 import { CiLogout } from 'react-icons/ci'
 import Link from 'next/link'
 import React from 'react'
@@ -9,6 +12,7 @@ import { userHooks } from '@/hooks'
 const Header = () => {
     const [user, setUser] = React.useState<any | null>(null)
     const [time, setTime] = React.useState<any | null>(new Date().toLocaleTimeString())
+    const [dropdown, setDropdown] = React.useState<boolean>(false)
 
     React.useEffect(() => {
         let user = userHooks({ type: 'get' })
@@ -56,20 +60,44 @@ const Header = () => {
                             <img className="w-10 h-10 rounded-full" src="https://codersquiz.netlify.app/img/bentil.jpeg" alt="user photo" />
                         </button>
                         {/* user info, name and potforlio */}
-                        <div className="flex flex-col items-start justify-center px-3 text-left gap-y-1" id="user-dropdown">
+                        <div onClick={() => setDropdown(!dropdown)} className="flex items-center gap-x-1 justify-center px-3 text-left gap-y-1" id="user-dropdown">
                             <span className="text-sm text-white font-medium">
                                 {user?.email || 'John Doe'}
                             </span>
+                            <div className="w-[1px] h-[10px] bg-gray-600"></div>
+                            <PiCaretDownBold className=' text-gray-500 hover:text-gray-200' />
                         </div>
                     </div>
 
                     {/* vertical line seperate */}
-                    <div className="w-[1px] h-[30px] bg-gray-300"></div>
                     {/* logout link */}
-                    <div onClick={Logout} className="flex cursor-pointer items-center justify-center gap-x-1 text-sm text-gray-300 hover:text-gray-400">
-                        <CiLogout className='w-5 h-5' />
-                        <span>Logout</span>
-                    </div>
+
+
+                    {/* dropdown */}
+                    {
+                        dropdown && (
+                            <div onClick={() => setDropdown(false)} className="w-[13%] absolute right-0 top-10 z-50 origin-top-right rounded-md shadow-lg px-1">
+                                <div className="bg-white rounded-md shadow-xs">
+                                    <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" id="user-dropdown">
+                                        {/* <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Switch Accouunt</a> */}
+                                        <div onClick={Logout} className="px-4 py-2 flex cursor-pointer gap-x-2 items-center justify-start text-sm text-gray-700 hover:bg-gray-100">
+                                            <PiUserSwitchThin className='w-5 h-5' />
+                                            <span>HOD View</span>
+                                        </div>
+                                        <div onClick={Logout} className="px-4 py-2 flex cursor-pointer gap-x-2 items-center justify-start text-sm text-gray-700 hover:bg-gray-100">
+                                            <AiOutlineSetting className='w-5 h-5' />
+                                            <span>Settings</span>
+                                        </div>
+                                        {/* <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Settings</a> */}
+                                        <div onClick={Logout} className="px-4 py-2 flex cursor-pointer gap-x-2 items-center justify-start text-sm text-gray-700 hover:bg-gray-100">
+                                            <CiLogout className='w-5 h-5' />
+                                            <span>Logout</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    }
 
                 </div>
             </div>
